2403906bcd1f47199ccff6b243d6fd3658de17bb
noname2601 <noname0328@gmail.com>
2021-04-28 18:19:37 +0900

    商品出品機能　一覧

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,34 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  has_one_attached :image
+  belongs_to :category
+  belongs_to :item_condition
+  belongs_to :shipping_free
+  belongs_to :area
+  belongs_to :ship_day
+  
+
+  with_options presence: true do
+    validates :selling_price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}
+    validates :name
+    validates :introduction
+    validates :category_id
+    validates :item_condition_id
+    validates :shipping_free_id
+    validates :area_id
+    validates :ship_day_id
+    validates :image
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :item_condition_id
+    validates :shipping_free_id
+    validates :area_id
+    validates :ship_day_id
+    validates :selling_price
+  end
+  
+
 end
+
---
+++ spec/factories/items.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :item do
+    name                   {"シャツ"}
+    introduction   {"おすすめ商品"}
+    category_id              {'2'}
+    item_condition_id        {'2'}
+    shipping_free_id         {'2'}
+    area_id                  {'2'}
+    ship_day_id              {'2'}
+    selling_price         {'1000'}
+    
+    
+  end
+end
