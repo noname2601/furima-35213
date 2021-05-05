f8cc2ca2060774e901322b9d05532f87ac164963
noname2601 <noname0328@gmail.com>
2021-05-05 18:13:57 +0900

    モデル修正

--- app/models/management_purchase.rb
+++ app/models/management_purchase.rb
@@ -1,12 +1,11 @@
 class ManagementPurchase
   include ActiveModel::Model
-  attr_accessor :post_code, :area_id, :city, :adress, :building_name, :phone_number, :management_id, :user_id, :item_id, :token
+  attr_accessor :post_code, :area_id, :city, :adress, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    #validates :phone_number, numericality: {only_integer: true, }
     VALID_PHONE_NUMBER_REGEX = /\A0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1})[-)]?\d{4}\z|\A0[5789]0[-]?\d{4}[-]?\d{4}\z/
-  validates :phone_number, presence: true, format: { with: VALID_PHONE_NUMBER_REGEX }, numericality: {only_integer: true, }
+  validates :phone_number, format: { with: VALID_PHONE_NUMBER_REGEX }, numericality: {only_integer: true, }
 
     validates :area_id
     validates :city
