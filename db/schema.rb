89077ad4c190d877fd4c1d7867ecc994b6e20e06
noname2601 <noname0328@gmail.com>
2021-05-05 16:32:54 +0900

    マイグレーションファイル更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_25_231257) do
+ActiveRecord::Schema.define(version: 2021_05_03_005153) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +43,25 @@ ActiveRecord::Schema.define(version: 2021_04_25_231257) do
     t.integer "ship_day_id", null: false
     t.integer "selling_price", null: false
     t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "managements", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "item_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "purchases", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "post_code", null: false
+    t.integer "area_id", null: false
+    t.string "city", null: false
+    t.string "adress", null: false
+    t.string "building_name"
+    t.string "phone_number", null: false
+    t.integer "management_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
